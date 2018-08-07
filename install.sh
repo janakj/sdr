2e1320f862aacf3787d553b16ef923936aa08112
Jan Janak <jan@janakj.org>
2018-08-07 19:59:18 -0400

    Add script to compile and install all the software.

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+root=$(pwd)
+prefix=/usr/local
+buildroot=$root/.build
+subprojects="SoapySDR LimeSuite gnuradio librtlsdr osmo-sdr/software/libosmosdr gr-iqbal gr-osmosdr gqrx"
+
+git submodule update --init --recursive
+
+for sub in $subprojects ; do
+    dir=$buildroot/$sub
+    mkdir -p $dir
+    pushd $dir
+
+    cmake CMAKE_INSTALL_PREFIX=$prefix $root/$sub
+    make
+
+    sudo make install
+    sudo ldconfig
+
+    popd
+done
