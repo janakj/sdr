d15ae680f92d573a7e8eab89d01551f72593edb8
Jan Janak <jan@janakj.org>
2018-08-08 00:17:02 -0400

    Add script to install development packages.

---
+++ install_debs.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+set -e
+
+apt-get install --yes --no-install-recommends \
+	cmake                                 \
+	build-essential                       \
+	doxygen                               \
+	graphviz                              \
+	texlive                               \
+	libpython-dev                         \
+	libpython3-dev swig
+
+# LimeSuite
+apt-get install --yes --no-install-recommends \
+	libusb-1.0-0-dev                      \
+	gnuplot                               \
+	libwxgtk3.0-dev                       \
+	libfltk1.3-dev                        \
+	freeglut3-dev                         \
+	libsqlite-dev                         \
+	libi2c-dev
+
+# gnuradio
+apt-get install --yes --no-install-recommends \
+	python-qt4                            \
+	libqwt-dev                            \
+	python-lxml                           \
+	python-cheetah                        \
+	liborc-0.4-dev                        \
+	python-wxgtk3.0-dev                   \
+	libcomedi-dev                         \
+	libfftw3-dev                          \
+	libuhd-dev                            \
+	libsdl-dev                            \
+	libgsl-dev                            \
+	python-mako                           \
+	libboost-all-dev                      \
+	pkg-config                            \
+	libcppunit-dev                        \
+	python-sphinx                         \
+	libzmq3-dev
+
+# gqrx
+apt-get install --yes --no-install-recommends \
+	libqt5svg5-dev                        \
+        qtbase5-dev
